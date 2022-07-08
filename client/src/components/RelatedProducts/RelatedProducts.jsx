7b2d2013d2286ba75afdbcd19fdac461f66eb918
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-07-08 12:38:53 -0500

    added mini server and reorganized

---
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+var RelatedProducts = () => {
+  return <div>RelatedProducts</div>;
+}
+
+export default RelatedProducts;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-import Overview from './components/Overview.jsx';
+import Overview from './components/Overview/Overview.jsx';
+import RelatedProducts from './components/RelatedProducts/RelatedProducts.jsx';
+import Reviews from './components/Reviews/Reviews.jsx';
 
 
 const root = createRoot(document.getElementById("root"));
@@ -9,6 +11,8 @@ const root = createRoot(document.getElementById("root"));
 const App = (props) => {
   return <>
     <Overview id={props.id} />
+    <RelatedProducts id={props.id} />
+    <Reviews id={props.id} />
   </>
 
 }
