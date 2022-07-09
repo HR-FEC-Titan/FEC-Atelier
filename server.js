576b2581cc2bd37f11034763fc14aab751e641fc
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-07-09 13:32:36 -0500

    add coverage to gitignore and added eslint

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/client/dist")));
 // other configuration...
 
+app.get('/:id', (req, res) => {
+  // pass id back to react to render
+})
+
 app.listen(3000, () => {
   console.log('Listening on 3000!');
 });
