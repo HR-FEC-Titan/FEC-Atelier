91e553b70e8d5887d9ae5b25b86430daa9c55a6c
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-07-09 22:42:57 -0500

    basic working 1st layer components

--- client/src/components/Overview/Style.jsx
+++ client/src/components/Overview/Style.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect, useContext, createContext } from 'react';
+import { StyleContext } from './Overview.jsx';
 
 
+let Style = () => {
+  const style = useContext(StyleContext);
+
+  return <div>
+    {style.map(s => {
+      <img href={s.photos.thumbnail_url}></img>
+    })}
+  </div>
+}
 
 export default Style;
