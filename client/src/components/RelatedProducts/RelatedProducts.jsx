d3eca40d2d414af0a2ededaa5d8f8fd4b27113f9
vpatel89 <virenpatel89@gmail.com>
2022-07-11 23:09:59 -0400

    price logic working

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -4,13 +4,13 @@ import axios from 'axios';
 // import bootstrap from 'bootstrap';
 
 var RelatedProducts = ({ id }) => {
-  const [relatedIDs, setRelatedIDs] = useState([]); //may not be necessary
+  // const [relatedIDs, setRelatedIDs] = useState([]); //may not be necessary
   const [relatedProdsData, setRelatedProdsData] = useState({});
 
   useEffect(() => {
     axios.get('/products/' + id + '/related')
       .then(res => {
-        setRelatedIDs(res.data); //may not be necessary
+        // setRelatedIDs(res.data); //may not be necessary
         res.data.forEach((relatedProdID) => {
           axios.get('/products/' + relatedProdID)
             .then(resData => {
@@ -60,15 +60,46 @@ var RelatedProducts = ({ id }) => {
     return convertedAverage;
   }
 
+  var defaultStylePrice = (styleData) => {
+    var originalPrice = styleData.find(element => element['default?'] === true).original_price;
+    var salePrice = styleData.find(element => element['default?'] === true).sale_price;
+
+    // console.log(originalPrice);
+    console.log(salePrice);
+    if (salePrice === null) {
+      return (
+        <div>
+          <span>{'$' + originalPrice}</span>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <span style={{color: "red"}} >
+            {'$' + salePrice + ' '}
+          </span>
+          <span>
+            <s>
+              {'$' + originalPrice}
+            </s>
+          </span>
+        </div>
+      )
+    }
+
+  }
+
   return (
     <div className='relatedProducts'>
       {Object.values(relatedProdsData).map((prodData) =>
         <div className="card" key={prodData.id} style={{width: "18rem"}} >
-          <img src={prodData.styles[0].photos[0].thumbnail_url} className="card-img-top" alt="..." />
+          <img src={prodData.styles[0].photos[0].url} className="card-img-top" alt="..." />
+          {/* <img src={defaultStylePic(prodData)} className="card-img-top" alt="..." /> */}
           <div className="card-body">
             <p className="card-text">{prodData.id}</p>
             <p className="card-text">{prodData.category}</p>
             <p className="card-text">{prodData.name}</p>
+            <div className="card-text">{defaultStylePrice(prodData.styles)}</div>
             <p className="card-text">{reviewAvg(prodData.reviews)}</p>
           </div>
         </div>
