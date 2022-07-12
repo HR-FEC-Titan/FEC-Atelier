6d7a75f7b47f5a2b3bde1569b3c9dcef9b5fd977
Fahad Syed <syed.216@gmail.com>
2022-07-12 12:27:56 -0500

    Finished list addmore button Functionality

--- client/src/components/Reviews/ReviewList.jsx
+++ client/src/components/Reviews/ReviewList.jsx
@@ -6,11 +6,10 @@ class ReviewList extends React.Component{
     super(props)
     this.state = {
       count: 2,
-      tot: this.props.reviews.length
     }
   }
   render() {
-  if(this.props.reviews.length > 2) {
+  if(this.props.reviews.length > 2 && this.state.count < this.props.reviews.length) {
     return (
       <div>
       {this.props.reviews.slice(0, this.state.count).map(review => {
@@ -18,9 +17,20 @@ class ReviewList extends React.Component{
       })}
       <button onClick = {()=>{
        this.setState({count: this.state.count+=2})
+       console.log(this.state.count)
       }}>MORE REVIEWS</button> <button>ADD A REVIEW</button>
       </div>
     )
+    }
+    if(this.state.count >= this.props.reviews.length) {
+      return (
+        <div>
+        {this.props.reviews.slice(0, this.state.count).map(review => {
+          return <ReviewTile key = {review.review_id} review={review}/>
+        })}
+       <button>ADD A REVIEW</button>
+        </div>
+      )
     } else {
       return (
         <div>
@@ -29,7 +39,6 @@ class ReviewList extends React.Component{
         })}
       <button>MORE REVIEWS</button>
         </div>
-
       )
     }
   }
